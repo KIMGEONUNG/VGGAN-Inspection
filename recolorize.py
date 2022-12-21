aaeba90d4332264a57f5a98f54537a12d6cc22d6
comar <saywooong@gmail.com>
2022-12-21 13:54:07 +0900

    complete: load hint in gui

--- recolorize.py
+++ recolorize.py
@@ -33,27 +33,39 @@ class ReColorGUI(object):
         # Define GUI Layout
         css = r"img { image-rendering: pixelated; }"
         with gr.Blocks(css=css) as self.demo:
-            with gr.Row():
-                view_gt = gr.Image(label="GT").style(height=self.height)
-                view_stroke = gr.ImagePaint(label="Stroke").style(
-                    height=self.height)
-            with gr.Row():
-                view_hint = gr.Image(
-                    label="hint", interactive=False).style(height=self.height)
-                view_overlay = gr.Image(
-                    label="overlay",
-                    interactive=False).style(height=self.height)
-            gr.Examples([
-                "inputs/ILSVRC2012_val_00002071_resize_256.JPEG",
-                "inputs/ILSVRC2012_val_00005567_resize_256_256.JPEG"
-            ],
-                        inputs=view_gt)
-            with gr.Row():
-                path_log = gr.Dropdown(choices=[
-                    "logs_mark/2022-12-18T07-06-50_chroma_vqgan",
-                    "logs_mark/2022-12-18T07-08-41_chroma_vqgan_randgray/"
-                ])
-                btn = gr.Button("Colorize").style(height=self.height)
+            with gr.Box():
+                with gr.Row():
+                    view_gt = gr.Image(label="GT").style(height=self.height)
+                    view_stroke = gr.ImagePaint(label="Stroke").style(
+                        height=self.height)
+                with gr.Row():
+                    view_hint = gr.Image(
+                        label="hint",
+                        interactive=True).style(height=self.height)
+                    view_overlay = gr.Image(
+                        label="overlay",
+                        interactive=False).style(height=self.height)
+                with gr.Row():
+                    upload_button = gr.UploadButton("Click to Upload a hint image", file_types=["image"], file_count="single")
+                gr.Examples([
+                    "inputs/ILSVRC2012_val_00002071_resize_256.JPEG",
+                    "inputs/ILSVRC2012_val_00005567_resize_256_256.JPEG"
+                ],
+                            inputs=view_gt)
+
+            with gr.Box():
+                with gr.Row():
+                    path_log = gr.Dropdown(choices=[
+                        "Baseline",
+                        "RandGray",
+                        "VQHint",
+                        "VQHint+RandGray",
+                    ])
+                    btn = gr.Button("Colorize").style(height=self.height)
+
+                with gr.Row():
+                    console = gr.Textbox(placeholder="No message",
+                                         interactive=False)
 
             with gr.Row():
                 view_output = gr.Image(
@@ -61,14 +73,31 @@ class ReColorGUI(object):
                     interactive=False).style(height=self.height)
 
             # Define GUI Events
+            upload_button.upload(self._upload_hint, inputs=[upload_button, view_gt], outputs=[view_hint, view_overlay])
             view_gt.change(self._togray, view_gt, view_stroke)
             view_stroke.change(self._mk_hint, [view_stroke, view_gt],
                                [view_hint, view_overlay])
-            path_log.change(self.set_model, inputs=[path_log])
+            path_log.change(self.set_model, inputs=[path_log], outputs=console)
             btn.click(self.predict, inputs=[view_gt], outputs=[view_output])
 
     def launch(self):
         self.demo.launch(share=self.share)
+
+    def _upload_hint(self, file, gt):
+        hint = skimage.io.imread(file.name)
+        self.hint = hint # Enroll hint
+
+        # Make overlay
+        alpha = 0.5
+        hint_up = cv2.resize(hint, gt.shape[:-1], interpolation=cv2.INTER_NEAREST)
+        mask = np.all(hint_up == [0, 0, 0], axis=-1)[..., None]
+
+        hint_up = hint_up.astype('float')
+        gt = gt.astype('float')
+        blend = gt * mask + (alpha * gt + (1 - alpha) * hint_up) * (1 - mask)
+        blend = blend.astype('uint8')
+
+        return hint, blend
 
     def _mk_hint(self, x, gt):
         if x is None or gt is None:
@@ -87,13 +116,12 @@ class ReColorGUI(object):
 
         # Make overlay
         alpha = 0.5
-        hint_up = cv2.resize(x, gt.shape[:-1],
-                             interpolation=cv2.INTER_NEAREST)
+        hint_up = cv2.resize(x, gt.shape[:-1], interpolation=cv2.INTER_NEAREST)
         mask = np.all(hint_up == [0, 0, 0], axis=-1)[..., None]
 
         hint_up = hint_up.astype('float')
         gt = gt.astype('float')
-        blend = gt *  mask + (alpha * gt + (1 - alpha) * hint_up) * (1 - mask)
+        blend = gt * mask + (alpha * gt + (1 - alpha) * hint_up) * (1 - mask)
         blend = blend.astype('uint8')
 
         return x, blend
@@ -133,7 +161,6 @@ class ReColorGUI(object):
         return g
 
     def _load_model(self, config, ckpt, gpu, eval_mode):
-        # Load the specified checkpoint
         if ckpt:
             pl_sd = torch.load(ckpt, map_location="cpu")
             global_step = pl_sd["global_step"]
@@ -147,7 +174,21 @@ class ReColorGUI(object):
                                        eval_mode=eval_mode)["model"]
         return model, global_step
 
-    def set_model(self, path_log):
+    def set_model(self, key):
+
+        MAP_PATH = {
+            "Baseline":
+            "logs_mark/2022-12-18T07-06-50_chroma_vqgan",
+            "RandGray":
+            "logs_mark/2022-12-18T07-08-41_chroma_vqgan_randgray",
+            "VQHint":
+            "logs_mark/2022-12-20T02-00-03_chroma_vqgan",
+            "VQHint+RandGray":
+            "logs_mark/2022-12-20T02-01-42_chroma_vqgan_randgray",
+        }
+
+        path_log = MAP_PATH[key]
+
         path_ckpt = glob(join(path_log, "checkpoints/*.ckpt"))[0]
         path_config = glob(join(path_log, "configs/*-project.yaml"))[0]
         assert exists(path_ckpt)
@@ -159,7 +200,9 @@ class ReColorGUI(object):
                                     gpu=True,
                                     eval_mode=True)
         self.model = model
-        print("New model was set : ", path_log)
+        message = "New model was set : ", path_log
+        print(message)
+        return message
 
     def predict(self, img: np.ndarray):
         """
