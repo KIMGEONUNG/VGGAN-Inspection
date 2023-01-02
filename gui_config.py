c3918911b8b63bc983d471b0a648faaacffa66ae
comar <saywooong@gmail.com>
2023-01-02 09:41:22 +0900

    src: change config

--- gui_config.py
+++ gui_config.py
@@ -16,7 +16,7 @@ MAP_PATH = {
     "VQHint+RandGray": "logs_mark/2022-12-20T02-01-42_chroma_vqgan_randgray",
     "VQHint+ScaleGray": "logs_mark/2022-12-21T12-25-22_chroma_vqgan",
     "VQHint+ScaleGrayIvt": "logs_mark/2022-12-26T03-16-40_chroma_vqgan",
-    "Hybrid+RandGray": "logs_mark/2022-12-18T07-08-41_chroma_vqgan_randgray",
+    "Hybrid+RandGray": "logs_mark/2022-12-29T02-19-18_chroma_vqgan/",
     "Hybrid+ScaleGray": "logs_mark/2022-12-28T02-04-41_chroma_vqgan/",
     "Hybrid+ScaleGrayIvt": "logs_mark/2022-12-27T02-03-01_chroma_vqgan",
 }
