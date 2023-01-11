9473e0ab3e00f78d43b887caf524fe9e5703e491
comar <saywooong@gmail.com>
2023-01-11 09:12:31 +0900

    feat: update configs

--- gui_config.py
+++ gui_config.py
@@ -4,11 +4,14 @@ CHOICES = [
     "VQHint",
     "VQHint+RandGray",
     "VQHint+ScaleGray",
+    "VQHint+ScaleGray+",
     "VQHint+ScaleGrayIvt",
     "VQHint+ScaleGray+CondGFeat",
     "Hybrid+RandGray",
     "Hybrid+ScaleGray",
+    "Hybrid+ScaleGray+",
     "Hybrid+ScaleGrayIvt",
+    "Hybrid+Shrink",
 ]
 
 MAP_PATH = {
@@ -16,9 +19,12 @@ MAP_PATH = {
     "VQHint": "logs_mark/2022-12-20T02-00-03_chroma_vqgan",
     "VQHint+RandGray": "logs_mark/2022-12-20T02-01-42_chroma_vqgan_randgray",
     "VQHint+ScaleGray": "logs_mark/2022-12-21T12-25-22_chroma_vqgan",
+    "VQHint+ScaleGray+": "logs_mark/2023-01-10T01-28-24_chroma_vqgan",
     "VQHint+ScaleGrayIvt": "logs_mark/2022-12-26T03-16-40_chroma_vqgan",
-    "Hybrid+RandGray": "logs_mark/2022-12-29T02-19-18_chroma_vqgan/",
-    "Hybrid+ScaleGray": "logs_mark/2022-12-28T02-04-41_chroma_vqgan/",
+    "Hybrid+RandGray": "logs_mark/2022-12-29T02-19-18_chroma_vqgan",
+    "Hybrid+ScaleGray": "logs_mark/2022-12-28T02-04-41_chroma_vqgan",
+    "Hybrid+ScaleGray+": "logs_mark/2023-01-09T13-48-43_chroma_vqgan",
     "Hybrid+ScaleGrayIvt": "logs_mark/2022-12-27T02-03-01_chroma_vqgan",
-    "VQHint+ScaleGray+CondGFeat": "logs_mark/2023-01-02T01-54-03_chroma_vqgan/",
+    "Hybrid+Shrink": "logs_mark/2023-01-04T04-16-24_chroma_vqgan",
+    "VQHint+ScaleGray+CondGFeat": "logs_mark/2023-01-02T01-54-03_chroma_vqgan",
 }
